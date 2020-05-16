7e0332fd89c2194da26bb1c7b3bdb1785136ffc4
Markelov Viktor <contact@marvic.ru>
2020-05-17 01:54:52 +0300

    build added next try =)

---
+++ build/precache-manifest.be445e0ebf034446174f9045d4d7ce9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58f59fe679fcd7d5aa96544258366b1b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9aed79c1cdab80b95b06",
+    "url": "/static/css/main.4bdaedb9.chunk.css"
+  },
+  {
+    "revision": "dec3f56d7903449200d1",
+    "url": "/static/js/2.d0cc3aca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d0cc3aca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9aed79c1cdab80b95b06",
+    "url": "/static/js/main.4dc32cc8.chunk.js"
+  },
+  {
+    "revision": "2b835ca2997d01b191d7",
+    "url": "/static/js/runtime-main.047f212b.js"
+  }
+]);
